dfad2444a72c92ff8c60b941c82cae3ad2d53ef8
brandon-stewart-rgb <brandon@pixeltree.us>
2021-08-21 15:00:59 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const months = ["January", "February", "March", "April", "May", "June", "July"];
+
+const random = Math.floor(Math.random() * months.length);
+console.log(random, months[random]);
+
+
+// source https://gist.github.com/ijmacdowell/8325491#file-adjectives-js
