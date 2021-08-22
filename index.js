f4d7ed59885d677d84cc0576be537dcac713f469
brandon-stewart-rgb <brandon@pixeltree.us>
2021-08-21 21:10:44 -0600

    arrays, js, html

--- index.js
+++ index.js
@@ -1,7 +1,62 @@
-const months = ["January", "February", "March", "April", "May", "June", "July"];
+const sixWord = document.getElementById('placer');
+var button = document.getElementById('button');
+var clear = document.getElementById('clear-button');
 
-const random = Math.floor(Math.random() * months.length);
-console.log(random, months[random]);
+
+const adverb1 = Math.floor(Math.random() * adverbsArray.length);
+const verb1 = Math.floor(Math.random() * verbsArray.length);
+
+
+
+const adverb2 = Math.floor(Math.random() * adverbsArray.length);
+const verb2 = Math.floor(Math.random() * verbsArray.length);
+const verb3 = Math.floor(Math.random() * verbsArray.length);
+
+const adverb3 = Math.floor(Math.random() * adverbsArray.length);
+
+
+const noun1 = Math.floor(Math.random() * nounsArray.length);
+const pluralNoun1 = Math.floor(Math.random() * pluralNounsArray.length);
+const pluralNoun2 = Math.floor(Math.random() * pluralNounsArray.length);
+
+
+//lists number 
+// console.log(random, adverbs[random]);
+
+//lists only word
+console.log()
+
+   
+
+// generate six word story
+button.addEventListener('click', function(e){
+    e.preventDefault();
+   return sixWord.innerHTML = (
+
+        adverbsArray[adverb1]  +' '+  
+
+        nounsArray[noun1] +', '+ 
+
+        verbsArray[verb1] +' '+  
+
+        pluralNounsArray[pluralNoun1]  +', '+ 
+
+        verbsArray[verb2] +' '+  
+
+        pluralNounsArray[pluralNoun2]
+
+        );
+});
+// clear six word story
+clear.addEventListener('click', function(){
+    
+  sixWord.innerHTML = '';
+
+   location.reload();
+
+});
+
+
 
 
 // source https://gist.github.com/ijmacdowell/8325491#file-adjectives-js
